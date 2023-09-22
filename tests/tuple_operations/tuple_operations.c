602f694a8f5d5e0e72c0619e949ebb09bd4ee0a2
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-09-22 18:45:34 -0300

    created tuple add

--- tests/tuple_operations/tuple_operations.c
+++ tests/tuple_operations/tuple_operations.c
@@ -1,7 +1,15 @@
 #include "../tester.h"
+#include "../../src/tuple_operations.c"
 
 #define suite_name tuple_operations
 #define test1_description CYAN"checks if two tuples are being added correctly"RESET
 #define test2_description CYAN"checks if two tuples are being subtracted correctly"RESET
 #define test3_description CYAN"checks if the values of a duple are each being inverted"RESET
+
+Test(suit_name, adding_two_tuples, .description = test1_description) {
+	t_tuple a = {1.2, 2.4, 3.5, 0};
+	t_tuple b = {1.2, 2.4, 3.5, 1};
+	t_tuple result = {2.4, 4.8, 7.0, 1};
+	cr_expect_arr_eq(result, add_tuples(a, b, result), sizeof(t_tuple));
+}
 
