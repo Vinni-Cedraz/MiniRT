46a1a33617b94f710efd01d33dfb07e79295ae24
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-09-24 15:18:15 -0300

    fixed compare_floats using fabs from math.h

--- tests/chapter_one/comparing_floats/compare_floats.c
+++ tests/chapter_one/comparing_floats/compare_floats.c
@@ -2,8 +2,42 @@
 
 #define suite_name float_tests
 
-Test(suite_name, comparing_floating_point_numbers) {
-	float a = 1.872;
-	float b = 1.872;
-	cr_expect_eq(1, compare_floats(a, b));
+Test(suite_name, comparing_equal_floats) {
+    float a = 1.872;
+    float b = 1.872;
+    cr_expect_eq(TRUE, compare_floats(a, b));
+}
+
+Test(suite_name, comparing_unequal_floats) {
+    float a = 1.872;
+    float b = 0.872;
+    cr_expect_eq(FALSE, compare_floats(a, b));
+}
+
+Test(suite_name, comparing_unequal_floats_below_one_above_zero) {
+    float a = 0.972;
+    float b = 0.872;
+    cr_expect_eq(FALSE, compare_floats(a, b));
+}
+
+Test(suite_name, comparing_unequal_floats_pretty_close) {
+    float a = 1.001;
+    float b = 1.000;
+    cr_expect_eq(FALSE, compare_floats(a, b));
+}
+
+Test(suite_name, comparing_unequal_floats_five_decimals,
+     .description = CYAN "\nFive decimal places was the highest precision detected: "
+                         "float_a(1.00001) != float_b(1.00000)") {
+    float a = 1.00001;
+    float b = 1.00000;
+    cr_expect_eq(FALSE, compare_floats(a, b));
+}
+
+Test(suite_name, comparing_unequal_floats_six_decimals,
+     .description = CYAN
+     "\nThis one should give a false positive, float_a(1.000001) == float_b(1.000000)") {
+    float a = 1.000001;
+    float b = 1.000000;
+    cr_expect_eq(TRUE, compare_floats(a, b));
 }
--- tests/chapter_one/putting_it_together/projectile.c
+++ tests/chapter_one/putting_it_together/projectile.c
@@ -1,4 +1,4 @@
-#include "tester.h"
+#include "../../tester.h"
 
 #define suite_name putting_it_together
 
@@ -6,13 +6,13 @@ typedef struct s_projectile {
     t_tuple position;
     t_tuple velocity;
 } t_projectile;
-
 typedef struct s_environment {
     t_tuple gravity;
     t_tuple wind;
 } t_environment;
 
-// returns a new projectile that represents the given projectile after one unit of time (one tick) has passed
+// returns a new projectile that represents the given projectile after one unit of time
+// (one tick) has passed
 t_projectile tick(t_environment env, t_projectile projectile) {
     t_projectile res;
 
@@ -22,18 +22,17 @@ t_projectile tick(t_environment env, t_projectile projectile) {
     return (res);
 }
 
-// projectile starts one unit above the origin.
-// velocity is normalized to 1 unit/tick.
-// p ← projectile(point(0, 1, 0), normalize(vector(1, 1, 0)))
-// gravity -0.1 unit/tick, and wind is -0.01 unit/tick.
-// e ← environmenThen, run tick repeatedly until the projectile’s y position is less
+// Run tick repeatedly until the projectile’s y position is less
 // than or equal to 0. Report the projectile’s position after each
 // tick, and the number of ticks it takes for the projectile to hit the
 // ground.t(vector(0, -0.1, 0), vector(-0.01, 0, 0))
 Test(suite_name, projectile_is_falling) {
-	t_projectile p = (t_projectile){.position = {}, .velocity = {}};
-	t_environment e = (t_environment){.gravity = {}, .wind = {}};
+    const t_environment e = {.gravity = {0, -0.1, 0}, .wind = {-0.01, 0, 0}};
+    t_projectile p = {.position = {0, 1, 0}, .velocity = {1, 1, 0}};
 
-	printf(CYAN"Projectile position: "RESET);
-	print_tuple(p.position);
+    do {
+        printf(CYAN "Projectile position: " RESET);
+        print_tuple(p.position);
+        p = tick(e, p);
+    } while ((!compare_floats(p.position[Y], 0.0000))); 
 }
