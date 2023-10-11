9d5a8faba03c9e14eed42540a02bec81fa5b89b2
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-10-11 11:17:57 -0300

    fix: was lacking POINT on "t_tuple expected" initializer
    modified:   rotation.c

--- tests/chapter_four/rotation/rotation.c
+++ tests/chapter_four/rotation/rotation.c
@@ -1,34 +1,24 @@
 #include "tester.h"
 
-// Scenario : Rotating a point half quarter around the x axis
-#define scenario1 CYAN "\n" \
-				"Given p ← point(0, 1, 0)\n" \
-				"And half_quarter ← create_x_rotation_matrix(π / 4)\n"              \
-				"Then half_quarter * p = point(0, 0.7071, 0.7071)\n"      \
-
-t_tuple p = {0, 1, 0, POINT};
-t_tuple res;
-
-Test(rotation, rotating_half_quarter_around_x_axis, .description = scenario1) {
-	const t_matrix half_quarter = create_x_rotation_matrix(M_PI / 4);
-	const t_tuple expected = {0, 0.7071, 0.7071};
-	multiply_tuple_by_matrix(p, half_quarter, res);
-	cr_expect_tuple_eq(res, expected);
-}
-
-// Scenario : Rotating a point a half and a full quarter around the x axis
+// Scenario : Rotating a point around the x axis
 #define scenario2 CYAN "\n" \
 				"Given p ← point(0, 1, 0)\n" \
+				"And half_quarter ← create_x_rotation_matrix(π / 4)\n"              \
 				"And full_quarter ← create_x_rotation_matrix(π / 2)\n"              \
+				"Then half_quarter * p = point(0, 0.707107, 0.707107)\n"                    \
 				"And full_quarter * p = point(0, 0, 1)" RESET
 
-Test(rotation, rotating_full_quarter_around_x_axis, .description = scenario2) {
+Test(rotation, rotating_around_x_axis, .description = scenario2) {
+	t_tuple res;
+	t_tuple p = {0, 1, 0, POINT};
 	const t_matrix half_quarter = create_x_rotation_matrix(M_PI / 4);
 	const t_matrix full_quarter = create_x_rotation_matrix(M_PI / 2);
-	const t_tuple expected = {0, 0, 1};
+	const t_tuple expected1 = {0, 0.707107, 0.707107, POINT};
+	const t_tuple expected2 = {0, 0, 1, POINT};
 	multiply_tuple_by_matrix(p, half_quarter, res);
-	multiply_tuple_by_matrix(res, full_quarter, res);
-	cr_expect_tuple_eq(res, expected);
+	cr_expect_tuple_eq(res, expected1);
+	multiply_tuple_by_matrix(p, full_quarter, res);
+	cr_expect_tuple_eq(res, expected2);
 }
 
 //Scenario : The inverse of an x-rotation rotates in the opposite direction
@@ -36,11 +26,13 @@ Test(rotation, rotating_full_quarter_around_x_axis, .description = scenario2) {
 				   "Given p ← point(0, 1, 0)\n"                      \
 				  "And half_quarter ← rotation_x(π / 4)\n"           \
 				  "And inv ← inverse(half_quarter)\n"                \
-				  "Then inv * p = point(0, √2/2, -√2/2)"RESET
+				  "Then inv * p = point(0, 0.707107, -0.707107)"RESET
 
 Test(rotation, the_inverse_of_the_rotation_matrix_rotates_in_the_opposite_direction, .description = scenario3) {
+	t_tuple res;
+	t_tuple p = {0, 1, 0, POINT};
 	const t_matrix half_quarter = create_x_rotation_matrix(M_PI / 4);
-	const t_tuple expected = {0, 0.7071, -0.7071};
+	const t_tuple expected = {0, 0.70710, -0.70710, POINT};
 	const t_matrix inverse_rotation = invert_matrix(half_quarter);
 	multiply_tuple_by_matrix(p, inverse_rotation, res);
 	cr_expect_tuple_eq(res, expected);
