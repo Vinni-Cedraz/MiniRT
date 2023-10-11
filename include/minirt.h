d25c5853c7b51c9487b24dbc82babe168c72bf87
astahjmo <contato.johnatan06@gmail.com>
2023-10-11 17:21:52 -0300

    feat: add test and feature to calculate scaling modified:   Makefile modified:   compile_commands.json modified:   include/minirt.h new file:   src/matrix_transformations/scaling.c modified:   tests/chapter_four/Makefile renamed:    tests/chapter_four/scalling/Makefile -> tests/chapter_four/scaling/Makefile renamed:    tests/chapter_four/scalling/compile_commands.json -> tests/chapter_four/scaling/compile_commands.json new file:   tests/chapter_four/scaling/scaling.c deleted:    tests/chapter_four/scalling/scalling.c

--- include/minirt.h
+++ include/minirt.h
@@ -122,4 +122,5 @@ t_matrix	create_matrix_of_cofactors(const t_matrix m);
 t_matrix	create_x_rotation_matrix(float r);
 t_matrix	create_y_rotation_matrix(float r);
 t_matrix	create_z_rotation_matrix(float r);
+t_matrix	create_scaling_matrix(const float x, const float y, const float z);
 #endif
