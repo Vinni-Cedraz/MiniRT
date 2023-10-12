8090844f65100fa1587d232aebee2e06a265be18
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-10-12 18:08:11 -0300

    drawing a whole clock
    successfully
    
    modified: tests/chapter_four/putting_it_together/CANVAS.ppm
    modified: tests/chapter_four/putting_it_together/putting_it_together.c

--- tests/chapter_four/putting_it_together/putting_it_together.c
+++ tests/chapter_four/putting_it_together/putting_it_together.c
@@ -10,39 +10,39 @@ t_tuple		twelve_o_clock = {
 		0, -450, 0, POINT };
 const t_tuple WHITE  = {1, 1, 1, COLOR};
 
-Test(putting_it_together, drawing_a_point_in_the_middle, .description = scenario1) {
+// Test(putting_it_together, drawing_a_point_in_the_middle, .description = scenario1) {
+//
+//  	t_canvas 		canvas;
+//
+// 	canvas = create_canvas(1080, 1920);
+// 	write_pixel(&canvas, twelve_o_clock[X], twelve_o_clock[Y], WHITE);
+// 	t_constr str = canvas_to_ppm(&canvas);
+//     create_ppm_file(str);
+// }
+//
+// #define scenario2 CYAN "\n Given canvas(1080, 1920)\n"  \
+// 					"And twelve_o'clock(0, -450)\n"            \
+// 					"And R <- create_z_rotation_matrix(π/6)\n" \
+// 					"Then translate_coordinate(twelve_o'clock * R) -> (one o clock)\n" RESET
 
- 	t_canvas 		canvas;
 
-	canvas = create_canvas(1080, 1920);
-	write_pixel(&canvas, twelve_o_clock[X], twelve_o_clock[Y], WHITE);
-	t_constr str = canvas_to_ppm(&canvas);
-    create_ppm_file(str);
-}
-
-#define scenario2 CYAN "\n Given canvas(1080, 1920)\n"  \
-					"And twelve_o'clock(0, -450)\n"            \
-					"And R <- create_z_rotation_matrix(π/6)\n" \
-					"Then translate_coordinate(twelve_o'clock * R) -> (one o clock)\n" RESET
-
-
-Test(putting_it_together, drawing_twelve_of_clock_and_one_o_clock, .description = scenario2) {
-
-	t_canvas 	canvas;
-	t_tuple		next_hour;
-
-	canvas = create_canvas(1080, 1920);
-	t_matrix Rz = create_z_rotation_matrix((M_PI / 6));
-
-	multiply_tuple_by_matrix(twelve_o_clock, Rz, next_hour);
-	translate_coordinate(next_hour, &canvas, next_hour);
-	translate_coordinate(twelve_o_clock, &canvas, twelve_o_clock);
-	write_pixel(&canvas, twelve_o_clock[Y], twelve_o_clock[X], WHITE);
-	write_pixel(&canvas, next_hour[Y], next_hour[X], WHITE);
-
-	t_constr str = canvas_to_ppm(&canvas);
-    create_ppm_file(str);
-}
+// Test(putting_it_together, drawing_twelve_of_clock_and_one_o_clock, .description = scenario2) {
+//
+// 	t_canvas 	canvas;
+// 	t_tuple		next_hour;
+//
+// 	canvas = create_canvas(1080, 1920);
+// 	t_matrix Rz = create_z_rotation_matrix((M_PI / 6));
+//
+// 	multiply_tuple_by_matrix(twelve_o_clock, Rz, next_hour);
+// 	translate_coordinate(next_hour, &canvas, next_hour);
+// 	translate_coordinate(twelve_o_clock, &canvas, twelve_o_clock);
+// 	write_pixel(&canvas, twelve_o_clock[Y], twelve_o_clock[X], WHITE);
+// 	write_pixel(&canvas, next_hour[Y], next_hour[X], WHITE);
+//
+// 	t_constr str = canvas_to_ppm(&canvas);
+//     create_ppm_file(str);
+// }
 
 #define scenario3 CYAN "\n Given canvas(1080, 1920)\n"  \
 					"And twelve_o'clock(0, -450)\n"            \
@@ -53,10 +53,25 @@ Test(putting_it_together, drawing_whole_clock, .description = scenario3) {
 
 	t_canvas 	canvas;
 	t_tuple		next_hour;
+	t_tuple		hour = {
+		0, -450, 0, POINT };
+	int i = 12;
 
 	canvas = create_canvas(1080, 1920);
 	t_matrix Rz = create_z_rotation_matrix((M_PI / 6));
 
+	translate_coordinate(twelve_o_clock, &canvas, twelve_o_clock);
+	write_pixel(&canvas, twelve_o_clock[Y], twelve_o_clock[X], WHITE);
+	while (--i)
+	{
+		multiply_tuple_by_matrix(hour, Rz, next_hour);
+		hour[X] = next_hour[X];
+		hour[Y] = next_hour[Y];
+		hour[Z] = next_hour[Z];
+		hour[W] = next_hour[W];
+		translate_coordinate(next_hour, &canvas, next_hour);
+		write_pixel(&canvas, next_hour[Y], next_hour[X], WHITE);
+	}
 	t_constr str = canvas_to_ppm(&canvas);
     create_ppm_file(str);
 }
