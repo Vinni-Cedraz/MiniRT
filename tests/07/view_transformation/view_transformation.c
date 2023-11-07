6215a6d1b133f02ce689fa8db683cf0b5a02951c
Vinni-Cedraz <planetexpress0101@gmail.com>
2023-11-07 18:00:17 -0300

    started view_transformation.c tests

--- tests/07/view_transformation/view_transformation.c
+++ tests/07/view_transformation/view_transformation.c
@@ -6,6 +6,9 @@
 "And up ← vector(0, 1, 0) \n" \
 "When t ← view_transform(from, to, up) \n" \
 "Then t = identity_matrix RESET\n"
+Test(view_transformation, default_orientation, .description = scenario1) {
+
+}
 
 // Scenario : A view transformation matrix looking in positive z direction
 #define scenario2 CYAN \
@@ -14,6 +17,9 @@
 "And up ← vector(0, 1, 0)\n"              \
 "When t ← view_transform(from, to, up)\n" \
 "Then t = scaling(-1, 1, -1)\n"RESET
+Test(view_transformation, positive_z_direction, .description = scenario2) {
+
+}
 
 // Scenario : The view transformation moves the world
 #define scenario3 CYAN \
@@ -22,6 +28,9 @@
 "And up ← vector(0, 1, 0)\n"                   \
 "When t ← view_transform(from, to, up)\n"      \
 "Then t = translation(0, 0, -8)\n"RESET
+Test(view_transformation, moves_the_world, .description = scenario2) {
+
+}
 
 // Scenario : An arbitrary view transformation
 #define scenario4 CYAN \
@@ -34,3 +43,6 @@
 "| 0.76772  | 0.60609 | 0.12122  | -2.82843 |\n"			 \
 "| -0.35857 | 0.59761 | -0.71714 | 0.00000  |\n"    		 \
 "| 0.00000  | 0.00000 | 0.00000  | 1.00000  |\n"RESET
+Test(view_transformation, arbitrary_transformation, .description = scenario3) {
+
+}
