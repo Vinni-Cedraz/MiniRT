a79716c5f3455c4426bd15406911ac8ec36fc49f
astahjmo <contato.johnatan06@gmail.com>
2023-11-09 20:12:28 -0300

    add render function

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:09:38 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/11/09 14:13:39 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/11/09 19:54:58 by johmatos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -309,5 +309,6 @@ t_matrix				view_transform(t_tuple from, t_tuple forward,
 t_camera				create_camera(int hsize, int vsize,
 							float field_of_view);
 t_ray					ray_for_pixel(t_camera c, int x, int y);
+t_canvas 				render(t_camera camera, t_world world);
 
 #endif
