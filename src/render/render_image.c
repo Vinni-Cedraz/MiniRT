e137857b3d86394e733305db60f095d001418a13
astahjmo <contato.johnatan06@gmail.com>
2023-11-30 11:41:40 -0300

    fix: norminette

--- src/render/render_image.c
+++ src/render/render_image.c
@@ -12,8 +12,9 @@
 
 #include "minirt.h"
 
-#define multi 255
-//TODO: fix the value 255 break the especular 
+#define MULTI 255
+
+// TODO: fix the value 255 break the especular
 uint32_t	normalized_color_to_int(t_tuple color)
 {
 	if (color[X] > 1)
@@ -22,18 +23,17 @@ uint32_t	normalized_color_to_int(t_tuple color)
 		color[Y] = 1;
 	if (color[Z] > 1)
 		color[Z] = 1;
-	return (((uint32_t)(color[X] * multi) << 24) |
-        ((uint32_t)(color[Y] * multi) << 16) |
-        ((uint32_t)(color[Z] * multi) << 8) |
-        0xff);
+	return (((uint32_t)(color[X] * MULTI) << 24) | ((uint32_t)(color[Y]
+				* MULTI) << 16) | ((uint32_t)(color[Z] * MULTI) << 8) | 0xff);
 }
 
-void	load_objs_into_world(mlx_image_t *image, t_camera camera, t_world *world)
+void	load_objs_into_world(mlx_image_t *image, t_camera camera,
+		t_world *world)
 {
-	t_ray		ray;
-	t_tuple		color;
-	int			x;
-	int			y;
+	t_ray	ray;
+	t_tuple	color;
+	int		x;
+	int		y;
 
 	x = 0;
 	y = 0;
@@ -53,10 +53,9 @@ void	load_objs_into_world(mlx_image_t *image, t_camera camera, t_world *world)
 
 mlx_image_t	**get_image_to_render(mlx_t *mlx)
 {
-	static mlx_image_t *image = NULL; 
+	static mlx_image_t	*image = NULL;
 
 	if (image == NULL)
 		image = mlx_new_image(mlx, sizeh, sizew);
-
 	return (&image);
 }
