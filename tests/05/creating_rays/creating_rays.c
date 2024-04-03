44bf209ed64090df55e428b56a99bba83675977b
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-04-03 18:42:05 -0300

    get_position passing

--- tests/05/creating_rays/creating_rays.c
+++ tests/05/creating_rays/creating_rays.c
@@ -23,8 +23,8 @@
 
 Test(creating_rays, create_and_query_a_ray, .description = scenario1)
 {
-	t_tuple 	origin = {1, 2, 3};
-	t_tuple		direction = {4, 5, 6};
+	t_tuple 	origin = {1, 2, 3, POINT};
+	t_tuple		direction = {4, 5, 6, VECTOR};
 	const t_ray	r = create_ray(origin, direction);
 
 	cr_expect_tuples_eq(r.origin, origin);
@@ -47,13 +47,12 @@ Test(creating_rays, point_from_distance, .description = scenario2)
 	);
 	t_tuple	result;
 
-	result = (t_tuple){0};
-	get_position(ray, 0, result);
+	result = get_position(ray, 0);
 	cr_expect_tuples_eq(result, (t_tuple){2, 3, 4, POINT});
-	get_position(ray, 1, result);
+	result = get_position(ray, 1);
 	cr_expect_tuples_eq(result, (t_tuple){3, 3, 4, POINT});
-	get_position(ray, -1, result);
+	result = get_position(ray, -1);
 	cr_expect_tuples_eq(result, (t_tuple){1, 3, 4, POINT});
-	get_position(ray, 2.5, result);
+	result = get_position(ray, 2.5);
 	cr_expect_tuples_eq(result, (t_tuple){4.5, 3, 4, POINT});
 }
