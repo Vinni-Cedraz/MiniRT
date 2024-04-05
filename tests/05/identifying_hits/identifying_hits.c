ac093ba485632001e0bf44a9016379c41828aa6a
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-04-05 18:33:35 -0300

    test ready

--- tests/05/identifying_hits/identifying_hits.c
+++ tests/05/identifying_hits/identifying_hits.c
@@ -11,15 +11,14 @@
 "Then hit = i1\n" RESET
 
 Test(identifying_hits, positive_t, .description = scenario1) {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(2, (t_shape **)&s_ptr),
-		intersection(1, (t_shape **)&s_ptr),
-		NULL
+	t_sphere s = create_sphere();
+
+	t_intersection i1 = intersection(2, &s);
+	t_intersection i2 = intersection(1, &s);
+	t_intersection *arr[] = {
+		&i1, &i2, NULL
 	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	const t_intersection hit = get_hit(arr);
 	cr_expect_eq(hit.t, 1);
 }
 
@@ -34,15 +33,14 @@ Test(identifying_hits, positive_t, .description = scenario1) {
 
 Test(identifying_hits, negative_t, .description = scenario2)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(1, (t_shape **)&s_ptr),
-		intersection(-1, (t_shape **)&s_ptr),
-		NULL
+	t_sphere s = create_sphere();
+	t_intersection i1 =	intersection(1, &s);
+	t_intersection i2 = intersection(-1, &s);
+	t_intersection *arr[] = {
+		&i1, &i2, NULL
 	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	
+	const t_intersection hit = get_hit(arr);
 	printf("%f\n", hit.t);
 	cr_expect_eq(hit.t, 1);
 }
@@ -58,15 +56,13 @@ Test(identifying_hits, negative_t, .description = scenario2)
 
 Test(identifying_hits, all_negatives, .description = scenario3)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = { 
-		intersection(-1, (t_shape **)&s_ptr),
-		intersection(-2, (t_shape **)&s_ptr),
-		NULL
+	t_sphere s = create_sphere();
+	t_intersection i1 = intersection(-1, &s);
+	t_intersection i2 = intersection(-2, &s);
+	t_intersection *arr[] = { 
+		&i1, &i2, NULL
 	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	const t_intersection hit = get_hit(arr);
 	cr_expect_eq(hit.object, NULL);
 }
 
@@ -83,17 +79,20 @@ Test(identifying_hits, all_negatives, .description = scenario3)
 
 Test(identifying_hits, hit_is_alway_lowest_nonnegative_intersection, .description = scenario4)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(5, (void *)&s_ptr),
-		intersection(7, (void *)&s_ptr),
-		intersection(-3, (void *)&s_ptr),
-		intersection(2, (void *)&s_ptr),
-		NULL
+	t_sphere s = create_sphere();
+	t_intersection i1 = intersection(5, &s);
+	t_intersection i2 = intersection(7, &s);
+	t_intersection i3 = intersection(-3,&s);
+	t_intersection i4 = intersection(2, &s);
+
+	t_intersection *arr[] = {
+			&i1,
+			&i2,
+			&i3,
+			&i4,
+			NULL
 	};
 
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	const t_intersection hit = get_hit(arr);
 	cr_expect_eq(hit.t, 2);
 }
