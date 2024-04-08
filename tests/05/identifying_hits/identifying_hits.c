388ea1d36dcc88eaa1846fec29a4d9f95bd2f6af
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-04-08 16:32:42 -0300

    fixed identifyting hits tests

--- tests/05/identifying_hits/identifying_hits.c
+++ tests/05/identifying_hits/identifying_hits.c
@@ -4,96 +4,84 @@
 // Scenario : The hit, when all intersections have positive t
 #define scenario1 CYAN \
 "Given s ← sphere()\n" \
-"And i1 ← intersection(1, s)\n" \
-"And i2 ← intersection(2, s)\n" \
+"And i1 ← ft_lstnew(1, s)\n" \
+"And i2 ← ft_lstnew(2, s)\n" \
 "And xs ← intersections(i2, i1)\n" \
 "When hit ← get_hit(xs)\n" \
 "Then hit = i1\n" RESET
 
 Test(identifying_hits, positive_t, .description = scenario1) {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(2, (t_shape **)&s_ptr),
-		intersection(1, (t_shape **)&s_ptr),
-		NULL
-	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	t_intersections intersections;
+	t_sphere s = create_sphere();
+	intersections.head = ft_lstnew(1, &s);
+	ft_lstadd_back(&intersections.head, ft_lstnew(2, &s));
+	intersections.count = 2;
+	const t_node hit = get_hit(intersections);
 	cr_expect_eq(hit.t, 1);
 }
 
 // Scenario : The hit, when some intersections have negative t
 #define scenario2 CYAN \
 "Given s ← sphere()\n" \
-"And i1 ← intersection(-1, s)\n" \
-"And i2 ← intersection(1, s)\n" \
+"And i1 ← ft_lstnew(-1, s)\n" \
+"And i2 ← ft_lstnew(1, s)\n" \
 "And xs ← intersections(i2, i1)\n" \
 "When hit ← get_hit(xs)\n" \
 "Then hit = i2\n" RESET
 
 Test(identifying_hits, negative_t, .description = scenario2)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(1, (t_shape **)&s_ptr),
-		intersection(-1, (t_shape **)&s_ptr),
-		NULL
-	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
-	printf("%f\n", hit.t);
+	t_sphere s = create_sphere();
+	t_intersections intersections;
+	intersections.head = ft_lstnew(1, &s);
+	ft_lstadd_back(&intersections.head, ft_lstnew(-1,&s));
+	intersections.count = 2;
+	const t_node hit = get_hit(intersections);
 	cr_expect_eq(hit.t, 1);
 }
 
 // Scenario : The hit, when all intersections have negative t
 #define scenario3 CYAN \
 "Given s ← sphere()\n" \
-"And i1 ← intersection(-2, s)\n" \
-"And i2 ← intersection(-1, s)\n" \
+"And i1 ← ft_lstnew(-2, s)\n" \
+"And i2 ← ft_lstnew(-1, s)\n" \
 "And xs ← intersections(i2, i1)\n" \
 "When hit ← get_hit(xs)\n" \
 "Then hit is nothing\n" RESET
 
 Test(identifying_hits, all_negatives, .description = scenario3)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = { 
-		intersection(-1, (t_shape **)&s_ptr),
-		intersection(-2, (t_shape **)&s_ptr),
-		NULL
-	};
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	t_sphere s = create_sphere();
+	t_intersections intersections;
+	intersections.head = ft_lstnew(-1, &s);
+	ft_lstadd_back(&intersections.head, ft_lstnew(-2, &s));
+	intersections.count = 2;
+	const t_node hit = get_hit(intersections);
 	cr_expect_eq(hit.object, NULL);
 }
 
-// Scenario : The hit is always the lowest nonnegative intersection
+// Scenario : The hit is always the lowest nonnegative ft_lstnew
 #define scenario4 CYAN \
 "Given s ← sphere()\n" \
-"And i1 ← intersection(5, s)\n" \
-"And i2 ← intersection(7, s)\n" \
-"And i3 ← intersection(-3, s)\n" \
-"And i4 ← intersection(2, s)\n" \
+"And i1 ← ft_lstnew(5, s)\n" \
+"And i2 ← ft_lstnew(7, s)\n" \
+"And i3 ← ft_lstnew(-3, s)\n" \
+"And i4 ← ft_lstnew(2, s)\n" \
 "And xs ← intersections(i1, i2, i3, i4)\n" \
 "When hit ← get_hit(xs)\n" \
 "Then hit = i4\n" RESET
 
 Test(identifying_hits, hit_is_alway_lowest_nonnegative_intersection, .description = scenario4)
 {
-	const t_sphere s = create_sphere();
-	const t_sphere *s_ptr = &s;
-	t_node *arr[] = {
-		intersection(5, (void *)&s_ptr),
-		intersection(7, (void *)&s_ptr),
-		intersection(-3, (void *)&s_ptr),
-		intersection(2, (void *)&s_ptr),
-		NULL
-	};
+	t_sphere s = create_sphere();
+	t_intersections intersections;
 
-	const t_intersection xs = link_intersection_nodes(arr);
-	const t_node hit = get_hit(xs);
+	intersections.head = ft_lstnew(5, &s);
+	ft_lstadd_back(&intersections.head, ft_lstnew(7, &s));
+	ft_lstadd_back(&intersections.head, ft_lstnew(-3, &s));
+	ft_lstadd_back(&intersections.head, ft_lstnew(2, &s));
+	intersections.count = 4;
+
+	const t_node hit = get_hit(intersections);
 	cr_expect_eq(hit.t, 2);
 }
