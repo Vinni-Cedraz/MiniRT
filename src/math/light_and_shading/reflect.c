35b54f799533acceae732cb6dd4dd75e0857a44a
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-04-09 15:27:27 -0300

    reflect vector is passing

--- src/math/light_and_shading/reflect.c
+++ src/math/light_and_shading/reflect.c
@@ -12,9 +12,13 @@
 
 #include "minirt.h"
 
-t_tuple reflect(t_tuple vector, t_tuple normal)
+t_tuple	reflect(t_tuple in, t_tuple normal)
 {
-	(void)vector;
-	(void)normal;
-	return (t_tuple){0};
+	t_tuple			reflected_vector;
+	const double	dot_prod = dot(in, normal);
+	const t_tuple	normal_by_dot = multiply_tuple_by_scalar(normal, dot_prod);
+	const t_tuple	expression = multiply_tuple_by_scalar(normal_by_dot, 2);
+
+	reflected_vector = subtract_tuples(in, expression);
+	return (reflected_vector);
 }
