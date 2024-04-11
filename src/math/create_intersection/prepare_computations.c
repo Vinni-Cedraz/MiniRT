87ed49e73c358681bca02239332b92870b9ebe23
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-04-11 07:36:35 -0300

    prep.inside implement

--- src/math/create_intersection/prepare_computations.c
+++ src/math/create_intersection/prepare_computations.c
@@ -21,5 +21,12 @@ t_prep_comps	prepare_computations(t_node *hit, t_ray ray)
 	prep.point = _intersection_coordinates(ray, prep.t);
 	prep.eyev = negate_tuple(ray.direction);
 	prep.normalv = sphere_normal_at(prep.object, prep.point);
+	if(dot(prep.normalv, prep.eyev) < 0)
+	{
+		prep.inside = true;
+		prep.normalv = negate_tuple(prep.normalv);
+	}
+	else
+		prep.inside = false;
 	return (prep);
 }
