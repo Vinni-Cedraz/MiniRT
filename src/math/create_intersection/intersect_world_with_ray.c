d9b767ecc3ec5c80d0fd7dc8515d5fff13c501f2
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-04-11 03:57:43 -0300

    intersect_world_ray norm

--- src/math/create_intersection/intersect_world_with_ray.c
+++ src/math/create_intersection/intersect_world_with_ray.c
@@ -6,14 +6,14 @@
 /*   By: vcedraz- <vcedraz-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 15:39:02 by vcedraz-          #+#    #+#             */
-/*   Updated: 2023/11/03 15:51:58 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2024/04/11 03:57:14 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static void insertionSort(t_node **head_ref);
-static void sortedInsert(t_node **head_ref, t_node *new_node);
+static void	insertion_sort(t_node **head_ref);
+static void	sorted_insert(t_node **head_ref, t_node *new_node);
 
 t_intersections	intersect_world_with_ray(t_world *w, t_ray *r)
 {
@@ -28,32 +28,44 @@ t_intersections	intersect_world_with_ray(t_world *w, t_ray *r)
 	{
 		tmp = intersect_sphere(&w->objs[idx], *r);
 		ft_lstadd_back(&result.head, tmp.head);
-		//printf("%f\n", tmp.head->next->t);
 		idx++;
 	}
 	result.count = w->count * 2;
-	insertionSort(&result.head);
+	insertion_sort(&result.head);
 	return (result);
 }
-static void insertionSort(t_node **head_ref) {
-	t_node *sorted = NULL;
-	t_node *current = *head_ref;
-	while (current != NULL) {
-		t_node *next = current->next;
-		sortedInsert(&sorted, current);
+
+static void	insertion_sort(t_node **head_ref)
+{
+	t_node	*sorted;
+	t_node	*current;
+	t_node	*next;
+
+	sorted = NULL;
+	current = *head_ref;
+	while (current != NULL)
+	{
+		next = current->next;
+		sorted_insert(&sorted, current);
 		current = next;
 	}
 	*head_ref = sorted;
 }
 
-static void sortedInsert(t_node **head_ref, t_node *new_node) {
-	t_node *current;
-	if (*head_ref == NULL || (*head_ref)->t >= new_node->t) {
+static void	sorted_insert(t_node **head_ref, t_node *new_node)
+{
+	t_node	*current;
+
+	if (*head_ref == NULL || (*head_ref)->t >= new_node->t)
+	{
 		new_node->next = *head_ref;
 		*head_ref = new_node;
-	} else {
+	}
+	else
+	{
 		current = *head_ref;
-		while (current->next != NULL && current->next->t < new_node->t) {
+		while (current->next != NULL && current->next->t < new_node->t)
+		{
 			current = current->next;
 		}
 		new_node->next = current->next;
