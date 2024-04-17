4a6fa5647b16a5ad30dd712817c97fe80c7b0c46
ISABELA GENIAL <isabelamgenial@gmail.com>
2024-04-17 16:25:05 -0300

    oi

--- src/math/light_and_shading/shadows.c
+++ src/math/light_and_shading/shadows.c
@@ -12,9 +12,33 @@
 
 #include "minirt.h"
 
+static t_node	calculate_hit(t_world *w, t_tuple p);
+
 t_bool	is_shadowed(t_world *w, t_tuple p)
 {
-	(void)w;
-	(void)p;
+	t_node	hit;
+	double	distance;
+
+	hit = calculate_hit(w, p);
+	distance = magnitude(w->light.position);
+	if (hit && hit.t < distance)
+		return (TRUE);
 	return (FALSE);
+}
+
+static t_node	calculate_hit(t_world *w, t_tuple p)
+{
+	double			distance;
+	t_node			hit;
+	t_tuple			vec;
+	t_tuple			direction;
+	t_intersections	intersections;
+
+	vec = w->light.position;
+	distance = magnitude(vec);
+	direction = normalize(vec);
+	r = create_ray(p, direction);
+	intersections = intersect_world_with_ray(w, r);
+
+	return (_hit(intersections));
 }
