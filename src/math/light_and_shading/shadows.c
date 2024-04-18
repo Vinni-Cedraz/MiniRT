b552ed0b3fc55e582b8c3565a24e5dbeeab15f78
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-04-18 15:32:50 -0300

    fixed is_shadowed function

--- src/math/light_and_shading/shadows.c
+++ src/math/light_and_shading/shadows.c
@@ -12,33 +12,15 @@
 
 #include "minirt.h"
 
-static t_node	calculate_hit(t_world *w, t_tuple p);
-
-t_bool	is_shadowed(t_world *w, t_tuple p)
+t_bool	is_shadowed(t_world *w, t_tuple point)
 {
-	t_node	hit;
-	double	distance;
+	const t_tuple	vec = subtract_tuples(w->light.position, point);
+	const double	distance = magnitude(vec);
+	const t_tuple	direction = normalize(vec);
+	const t_ray		r = create_ray(point, direction);
+	const t_node	hit = _hit(intersect_world_with_ray(w, (t_ray *)&r));
 
-	hit = calculate_hit(w, p);
-	distance = magnitude(w->light.position);
-	if (hit && hit.t < distance)
+	if (hit.object && hit.t < distance)
 		return (TRUE);
 	return (FALSE);
-}
-
-static t_node	calculate_hit(t_world *w, t_tuple p)
-{
-	double			distance;
-	t_node			hit;
-	t_tuple			vec;
-	t_tuple			direction;
-	t_intersections	intersections;
-
-	vec = w->light.position;
-	distance = magnitude(vec);
-	direction = normalize(vec);
-	r = create_ray(p, direction);
-	intersections = intersect_world_with_ray(w, r);
-
-	return (_hit(intersections));
 }
